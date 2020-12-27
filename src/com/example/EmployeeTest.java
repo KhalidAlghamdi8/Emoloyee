15f9d1b9dc83a546242874e46204a2b82fae31cf
KhalidAlghamdi8 <khalid.alghamdi1055@gmail.com>
2020-12-27 23:45:24 +0300

    Initial commit of all files to the repository

--- src/com/example/EmployeeTest.java
+++ src/com/example/EmployeeTest.java
@@ -9,7 +9,7 @@ import com.example.domain.Employee;
 
 /**
  *
- * @author khali
+ * @author khalid
  */
 public class EmployeeTest {
 
@@ -18,14 +18,66 @@ public class EmployeeTest {
      */
     public static void main(String[] args) {
        Employee emp = new Employee();
+       emp.setEmpId(101);
+       emp.setName("Jane Smith");
+       emp.setSnn("123-333-44");
+       emp.setSalary(120_321.23);
+       
 
+       Employee man = new Employee();
+       man.setEmpId(207);
+       man.setName("Barbara Johnson");
+       man.setSnn("111-333-44");
+       man.setSalary(109_321.23);
+       man.setDeptName("US Marketing");
        
+       Employee admin = new Employee();
+       admin.setEmpId(307);
+       admin.setName("Bill Munroe");
+       admin.setSnn("155-333-44");
+       admin.setSalary(10_321.23);
        
+       Employee dir = new Employee();
+       dir.setEmpId(12);
+       dir.setName("Susan Wheeler");
+       dir.setSnn("111-333-44");
+       dir.setSalary(199_321.23);
+       dir.setDeptName("Global Markteing");
+       dir.setBudget(1_000_000.00);
+       
+
+        System.out.println("Enginer");
         System.out.println("Employee id:     " + emp.getEmpId());
-        System.err.println("Employee name:   " + emp.getName());
+        System.out.println("Employee name:   " + emp.getName());
         System.out.println("Employee Soc Sec #: " + emp.getSnn());
         System.out.println("Employee salary:   " + emp.getSalary());
         
+        System.out.println("\n");
+        System.out.println("Manger");
+        System.out.println("Employee id:     " + man.getEmpId());
+        System.out.println("Employee name:   " + man.getName());
+        System.out.println("Employee Soc Sec #: " + man.getSnn());
+        System.out.println("Employee salary:   " + man.getSalary());
+        System.out.println("Employee DeptName:   " + man.getDeptName());
+
+        
+        System.out.println("\n");
+        System.out.println("Admin");
+        System.out.println("Employee id:     " + admin.getEmpId());
+        System.out.println("Employee name:   " + admin.getName());
+        System.out.println("Employee Soc Sec #: " + admin.getSnn());
+        System.out.println("Employee salary:   " + admin.getSalary());
+        
+        System.out.println("\n");
+        System.out.println("Director");
+        System.out.println("Employee id:     " + dir.getEmpId());
+        System.out.println("Employee name:   " + dir.getName());
+        System.out.println("Employee Soc Sec #: " + dir.getSnn());
+        System.out.println("Employee salary:   " + dir.getSalary());
+        System.out.println("Employee DeptName:   " + dir.getDeptName());
+        System.out.println("Employee Budget:   " + dir.getBudget());
+
+
     }
     
 }
--- src/com/example/domain/Employee.java
+++ src/com/example/domain/Employee.java
@@ -17,16 +17,84 @@ public class Employee {
     private String name;
     private String snn;
     private double salary;
+    private String deptName;
+    private double budget;
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+    
+    static String printEmployee (String Employee) {
+        System.out.println(Employee);
+        return Employee;            
+}
+
     
     
     
+    class Manager {
+
+    private String deptName;
+
+        
+        class Director {
+            private double budget;
+            private String deptName;
+
+            public double getBudget() {
+                return budget;
+            }
+
+            public String getDeptName() {
+                return deptName;
+            }
+
+            public int getEmpId() {
+                return empId;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public String getSnn() {
+                return snn;
+            }
+
+            public double getSalary() {
+                return salary;
+            }
+        }
+    }
+
+    class Engineer {
+
+    }
+
+    class Admin {
+
+    }
     public int getEmpId() {
         return empId;
     }
+    
 
-//    public void setEmpId(int empId) {
-//        this.empId = empId;
-//    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
 
     public String getName() {
         return name;
@@ -40,17 +108,22 @@ public class Employee {
         return snn;
     }
 
-//    public void setSnn(String snn) {
-//        this.snn = snn;
-//    }
+    public void setSnn(String snn) {
+        this.snn = snn;
+    }
 
     public double getSalary() {
         return salary;
     }
 
-//    public void setSalary(double salary) {
-//        this.salary = salary;
-//    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+static double raiseSalary (double increase){
+    return 0;
+}
+
 
 
 }
